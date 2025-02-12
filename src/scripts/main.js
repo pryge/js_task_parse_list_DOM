81e1e0ba51b9ca3b182563739a01f8f80ccc409c
Oleh <pryge321@gmail.com>
2025-02-12 12:14:13 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', function () {
+  function sortList() {
+    const list = document.querySelector('ul');
+    const items = Array.from(list.querySelectorAll('li'));
+
+    function getSalaryNumbers(salary) {
+      return Number(salary.replace(/[$,]/g, ''));
+    }
+
+    items.sort(
+      (a, b) =>
+        getSalaryNumbers(b.dataset.salary) - getSalaryNumbers(a.dataset.salary),
+    );
+
+    items.forEach((item) => list.appendChild(item));
+  }
+
+  sortList();
+});
