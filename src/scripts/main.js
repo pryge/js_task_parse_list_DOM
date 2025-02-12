da25657c87d0458dea9412177e219bd9abcb153b
Oleh <pryge321@gmail.com>
2025-02-12 16:59:04 +0200

    solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,17 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
+  function getEmployees() {
+    const listItems = document.querySelectorAll('ul li');
+
+    return Array.from(listItems).map((item) => ({
+      name: item.textContent.trim(),
+      position: item.dataset.position,
+      salary: Number(item.dataset.salary.replace(/[$,]/g, '')),
+      age: Number(item.dataset.age),
+    }));
+  }
+
   function sortList() {
     const list = document.querySelector('ul');
     const items = Array.from(list.querySelectorAll('li'));
@@ -17,5 +28,6 @@ document.addEventListener('DOMContentLoaded', function () {
     items.forEach((item) => list.appendChild(item));
   }
 
+  getEmployees();
   sortList();
 });
